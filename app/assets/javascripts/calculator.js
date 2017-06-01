b8907ec637714055d9662fbd6a0d2a6f803da55f
Jack Anderson <phrygian.jack@gmail.com>
2017-06-01 17:25:14 +0000

    Got RPN script to console log correct output

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -1,4 +1,48 @@
 $(function() {
+  // Find the form on the page and get ready to do stuff when the submit button is clickec
+  var expression = $('#new-form').submit(function(event) {
+    // Stop the form from refreshing the page
+    event.preventDefault();
+    // Extract the string from the form box
+    var expression = $('.calculation');
 
-  
-})
+    // Do the calculations
+    var calc = (expression.val().split(' '));
+    var stack = [];
+    for (var i = 0, l = calc.length; i<l; i++){
+      var e = calc[i];
+      if (e == '+'){
+        let val1 = parseInt(stack.pop(), 10);
+        let val2 = parseInt(stack.pop(), 10);
+        let newValue = val2 + val1;
+        stack.push(newValue);
+        }
+      else 
+        if ( e == '-' ) {
+          let val1 = parseInt(stack.pop(), 10);
+          let val2 = parseInt(stack.pop(), 10);
+          let newValue = val2 - val1;
+          stack.push(newValue);
+        }
+      else
+        if ( e == '*' ) {
+          let val1 = parseInt(stack.pop(), 10);
+          let val2 = parseInt(stack.pop(), 10);
+          let newValue = val2 * val1;
+          stack.push(newValue);
+        }
+      else
+        if ( e == '/' ) {
+          let val1 = parseInt(stack.pop(), 10);
+          let val2 = parseInt(stack.pop(), 10);
+          let newValue = val2 / val1;
+          stack.push(newValue);
+        }
+      else {
+        stack.push(e);
+      }
+    } 
+    // return parseInt(stack.pop(), 10);
+    console.log(parseInt(stack.pop(), 10))
+  });
+});
